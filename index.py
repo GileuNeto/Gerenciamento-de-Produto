812526af929b85abe0a2df180244c08fff196ce0
Gileu Neto <gileuneto@gmail.com>
2023-08-21 00:40:20 -0300

    resolvento erro

--- index.py
+++ index.py
@@ -18,12 +18,12 @@ dados =[]
 Titulos = ['Lote', 'Produto', 'Fornecedor']
 
 layout = [
-    [sg.Text(Titulos[0]), sg.Input(size= 5, key=Titulos[0])]
-    [sg.Text(Titulos[1]), sg.Input(size= 20, key=Titulos[1])], 
-    [sg.Text(Titulos[2]), sg.Combo(['Fornecedo 1', 'Fornecedor 2', 'Fornecedor 3'], key=Titulos[2])],
-    [sg.Button('Adicionar'), sg.Button('Editar'), sg.Button('Salvar' disable=True), sg.Button('Excluir'), sg.Exit('Sair')],
-    [sg.Table(dados, Titulos, key='tabela')]
-    ]
+        [sg.Text(Titulos[0]), sg.Input(size= 5, key=Titulos[0])],
+        [sg.Text(Titulos[1]), sg.Input(size= 20, key=Titulos[1])], 
+        [sg.Text(Titulos[2]), sg.Combo(['Fornecedo 1', 'Fornecedor 2', 'Fornecedor 3'], key=Titulos[2])],
+        [sg.Button('Adicionar'), sg.Button('Editar'), sg.Button('Salvar'), sg.Button('Excluir'), sg.Exit('Sair')],
+        [sg.Table(dados, Titulos, key='tabela')]
+        ]
 
 window = sg.Window('Sistema de Gerenciamento  de Suplementos', layout)
 
